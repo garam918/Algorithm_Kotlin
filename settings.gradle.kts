35bae8f94addd9650bdcc04ae0004e29eab2145a
garam918 <62510764+garam918@users.noreply.github.com>
2021-02-25 23:18:39 +0900

    BOJ_2606

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Algorithm_Kotlin"
+
---
+++ src/main/kotlin/BOJ/BOJ_2606.kt
@@ -0,0 +1,38 @@
+package BOJ
+
+import java.util.*
+
+fun main(){
+    val input = readLine()
+    val input2 = readLine()
+    val a = input!!.toInt()
+    val b = input2!!.toInt()
+    var c = 0
+    val map = Array(a+1) { IntArray(a + 1) }
+    val visit = Array(a+1){ false }
+
+    for (m in 0 until b) {
+        val st = readLine()!!.split(" ")
+        val start = st[0].toInt()
+        val end = st[1].toInt()
+        map[start][end] = 1
+        map[end][start] = 1
+    }
+
+    val q : Queue<Int> = LinkedList()
+    q.offer(1)
+
+    while (!q.isEmpty()) {
+        val x = q.poll()
+
+        for ( i in 0 .. a ){
+            if (map[x][i] == 1 && !visit[i]!!){
+                q.offer(i)
+                visit[i] = true
+                c++
+            }
+        }
+
+    }
+    println(c-1)
+}
