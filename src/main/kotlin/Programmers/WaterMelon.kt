f19d35e2d93f2953fb7c702933ba7a0490129ed1
garam918 <62510764+garam918@users.noreply.github.com>
2021-03-31 22:00:30 +0900

    Programmers_Level 1_수박수박수박수박수박수?

---
+++ src/main/kotlin/Programmers/WaterMelon.kt
@@ -0,0 +1,28 @@
+package Programmers
+
+fun main() {
+    solution(4)
+}
+
+private fun solution(n: Int): String {
+    var answer = ""
+
+    val arrayList = ArrayList<String> ()
+
+    for(i in 1 .. n) {
+        when(i % 2) {
+            0 -> {
+                arrayList.add("박")
+            }
+
+            1 -> {
+                arrayList.add("수")
+            }
+        }
+    }
+
+    answer = arrayList.joinToString().replace(",","").replace(" ","")
+    println(answer)
+
+    return answer
+}
