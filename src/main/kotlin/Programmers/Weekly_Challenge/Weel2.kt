bc36f0e2f237875fda6bbace5e2a4dde23f0659a
garam918 <62510764+garam918@users.noreply.github.com>
2021-08-10 02:01:12 +0900

    Programmers_Weekly_Challenge - Week2

---
+++ src/main/kotlin/Programmers/Weekly_Challenge/Weel2.kt
@@ -0,0 +1,44 @@
+package Programmers.Weekly_Challenge
+
+fun main() {
+    println(solution(arrayOf(intArrayOf(70,49,90), intArrayOf(68,50,38), intArrayOf(73,31,100))))
+}
+
+private fun solution(scores: Array<IntArray>): String {
+    var answer = ""
+
+
+    for (i in scores.indices) {
+        var count = 0
+        val list = MutableList(scores[i].size) { 0 }
+
+        repeat(scores[i].size) {
+            list[it] = scores[it][i]
+        }
+
+        answer += if(list.min() == scores[i][i] || list.max() == scores[i][i]) {
+            list.sorted().forEachIndexed { index, int ->
+                if (scores[i][i] == int) count++
+            }
+            if (count >= 2) avgToGrade(list.average().toInt())
+            else {
+                list.remove(scores[i][i])
+                avgToGrade(list.average().toInt())
+            }
+        }
+        else avgToGrade(list.average().toInt())
+
+    }
+
+    return answer
+}
+
+private fun avgToGrade(score : Int) : String {
+    return when(score) {
+        in 90 .. 100 -> "A"
+        in 80 until 90 -> "B"
+        in 70 until 80 -> "C"
+        in 50 until 70 -> "D"
+        else -> "F"
+    }
+}
