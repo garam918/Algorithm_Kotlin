cf442597bff3c8e38977f5abff8f50f53eadc993
garam918 <62510764+garam918@users.noreply.github.com>
2022-09-25 20:26:04 +0900

    BOJ_17175 - 떡 먹는 호랑이

---
+++ src/main/kotlin/BOJ/DP/BOJ_2502.kt
@@ -0,0 +1,22 @@
+package BOJ.DP
+
+fun main() {
+    val (d,k) = readln().split(" ").map { it.toInt() }
+
+    val x = dp(d-2)
+    val y = dp(d-1)
+
+    loop@
+    for(i in 1 .. 100000) {
+        for(j in i .. 100000) {
+            if(((x * i) + (y * j)) == k) {
+                println(i)
+                println(j)
+                break@loop
+            }
+        }
+    }
+}
+
+private fun dp(x: Int) : Int = if(x <= 2) 1
+else dp(x-1) + dp(x-2)
